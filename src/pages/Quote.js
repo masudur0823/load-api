19c1e707abc67b6de92cffd8f8f1ea93245cdaef
masudur0823 <masudur0823@gmail.com>
2023-11-22 17:50:26 +0600

    e

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,29 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Spinner } from "react-bootstrap";
 import { useQuery } from "react-query";
-import { getQuoteApi } from '../componets/api/axios'
+import { getQuoteApi } from "../componets/api/axios";
 
 function Quote() {
-  const { data, isLoading, isError, error } = useQuery("quote", getQuoteApi);
+  const { data, isLoading, isError, error, refetch } = useQuery("quote", getQuoteApi, {
+    refetchOnWindowFocus:false
+  });
   const quote = data?.data;
-  
-  if (isLoading) {
-    return (
-      <div
-        className="d-flex align-items-center justify-content-center"
-        style={{ height: "100vh" }}
-      >
-        <Spinner animation="grow" variant="success" />
-      </div>
-    );
-  }
+
+  const [count, setCount] = useState(10);
+
+  useEffect(() => {
+     setTimeout(() => {
+      if (count > 0) {
+        setCount(count - 1);
+      }
+    }, 1000);
+
+    if (count === 0) {
+      refetch()
+      setCount(10);
+    }
+  }, [count]);
+
   if (isError) {
     return <h1>{error.message}</h1>;
   }
+
   return (
     <Container>
+   
       <h1>Quotable Api</h1>
-      <p>{quote?.content}</p>
+      <p>get new data after {count}s</p>
+      {isLoading ? (
+        <div
+          className="d-flex align-items-center justify-content-center"
+          style={{ height: "100vh" }}
+        >
+          <Spinner animation="grow" variant="success" />
+        </div>
+      ) : (
+        <p>{quote?.content}</p>
+      )}
     </Container>
   );
 }
